49a07b9feb5dbec710bd98dc139a568e093a32a3
mallorybelgica <mcbcreativeco@gmail.com>
2022-09-01 15:17:48 -0400

    moved files in backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Backend Server is working");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
