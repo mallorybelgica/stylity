68cab774171ba4dd4481302e7acdac447707390f
mallorybelgica <mcbcreativeco@gmail.com>
2022-09-28 14:45:08 -0400

    made small change to server url

--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { REACT_APP_SERVER_URL } from "@env";
 console.log({ REACT_APP_SERVER_URL });
 const instance = axios.create({
-  baseURL: "http://192.168.2.15:8000",
+  baseURL: "https://stylity-514.herokuapp.com",
 });
 
 export default instance;
